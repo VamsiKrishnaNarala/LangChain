7b2cd7d3fd62de224bdbe3db20f91ea156af0e37
VamsiKrishnaNarala <108533648+VamsiKrishnaNarala@users.noreply.github.com>
2025-04-02 15:24:43 +0530

    Update app.py

--- LangChain/app.py
+++ LangChain/app.py
@@ -9,10 +9,7 @@ os.environ["OPENAI_API_KEY"]=openapi_key
 st.title("CHATBOT")
 input_text=st.text_input("Enter Topic You Want")
 
-llm=OpenAI(
-    base_url="https://openrouter.ai/api/v1",
-    api_key=openapi_key,
-    temperature=0.8)
+llm=OpenAI(temperature=0.8)
 
 if input_text:
     st.write(llm(input_text))
